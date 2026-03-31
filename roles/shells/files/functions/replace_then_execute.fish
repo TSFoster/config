05985d1c8696799066a0a8469b0423ab0a78cde2
T.S. Foster <hi@tsf.io>
2026-03-31 10:46:57 +0100

    Improve paging

--- roles/shells/files/functions/replace_then_execute.fish
+++ roles/shells/files/functions/replace_then_execute.fish
@@ -1,4 +1,3 @@
-# TODO anywhere abbr (abbr --add T --position anywhere '| rg -s')
 function replace_then_execute
   if set --query prompt_count
     set -l new_command ( \
@@ -6,11 +5,8 @@ function replace_then_execute
         | sed 's/ N$/; terminal-notifier -message "Command finished"/g' \
         | sed 's/ G / | rg -S /g' \
     )
-    if [ "$NVIM" ]
-      set new_command (echo $new_command | sed 's/ V$/ | nvr +PagerInit --remote-tab-wait -/g')
-    else
-      set new_command (echo $new_command | sed 's/ V$/ | '"$PAGER"'/g')
-    end
+    set new_command (echo $new_command | sed 's/ V$/ | '"$PAGER"'/g')
+
     commandline -r $new_command
 
     set prompt_count (expr $prompt_count + 1)
